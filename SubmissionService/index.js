92715c18814fd1fc444556dc4de0e7f1ffbd7b34
thanhta <borntobeagymer7@gmail.com>
2024-05-31 15:26:01 +0700

    init submission service

---
+++ SubmissionService/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const route = require("./routes");
+
+app.use(cors({
+    origin: '*',
+    allowMethods: ["GET", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"],
+    allowHeaders: [
+        "DNT",
+        "User-Agent",
+        "X-Requested-With",
+        "If-Modified-Since",
+        "Cache-Control",
+        "Content-Type",
+        "Range",
+        "authentication",
+        "Authorization"
+    ],
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.HOST_PORT || 6000;
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello World!" });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.use('/api', route);
